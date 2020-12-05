8c83f6d976c058df2d8967f7e06fb9090cce1c12
Robson Paiva <paivawork@hotmail.com>
2020-12-05 15:17:22 -0300

    Criação das tabelas e colunas com uso do EF Core Code-First

---
+++ src/Api.Data/Context/MyContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Api.Domain.Entities;
+using Api.Data.Mapping;
+
+namespace Api.Data.Context
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+
+        public MyContext(DbContextOptions<MyContext> option) : base(option) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UserEntity>(new UserMap().Configure);
+        }
+    }
+}
